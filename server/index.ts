28a22a248823de5aecb46ae98ba8ac1756790c10
Gnanendrakumarh <gnanendrakumarh2@gmail.com>
2025-08-08 21:59:29 +0530

    doggodex

--- server/index.ts
+++ server/index.ts
@@ -61,9 +61,15 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   // const port = parseInt(process.env.PORT || '5000', 10);
-  const port = parseInt(process.env.PORT || '5000', 10);
-  const host = process.env.NODE_ENV === "development" ? "127.0.0.1" : "0.0.0.0";
- server.listen({ port, host }, () => {
-  log(`serving on http://${host}:${port}`);
+  const port = parseInt(process.env.PORT || '8080', 10);
+const host = process.env.NODE_ENV === "development" ? "127.0.0.1" : "0.0.0.0";
+
+server.listen({ port, host }, () => {
+  if (process.env.NODE_ENV === "development") {
+    log(`Serving locally at http://${host}:${port}`);
+  } else {
+    log(`Server running on port ${port}`);
+  }
 });
+
 })();
