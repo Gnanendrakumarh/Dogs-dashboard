1cef0bfe8eb8059b207dda0b6f0650d53c16a152
Gnanendrakumarh <gnanendrakumarh2@gmail.com>
2025-08-09 10:48:39 +0530

    /db connection

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,20 @@
-import "dotenv/config";
-import { Pool } from "pg";
+// import "dotenv/config";
+// import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import * as schema from "@shared/schema";
+
+// const connectionString = process.env.DATABASE_URL;
+// if (!connectionString) {
+//   throw new Error("DATABASE_URL must be set.");
+// }
+
+// export const pool = new Pool({ connectionString });
+// export const db = drizzle(pool, { schema });
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "@shared/schema";
+import pg from "pg";
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL must be set.");
-}
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-export const pool = new Pool({ connectionString });
-export const db = drizzle(pool, { schema });
+const db = drizzle(pool);
