d3870f421faa615e3fed592c5f674e5536789d4e
Gnanendrakumarh <gnanendrakumarh2@gmail.com>
2025-08-09 10:40:16 +0530

    dep

--- server/index.ts
+++ server/index.ts
@@ -61,8 +61,6 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   // const port = parseInt(process.env.PORT || '5000', 10);
-
-  
   const port = parseInt(process.env.PORT || '8080', 10);
 
 // In production, always bind to 0.0.0.0
